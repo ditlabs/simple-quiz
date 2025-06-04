bd38e6afde1e87e8b964a5d71ada9f03633193ae
rdityaa <radityarizki2309@gmail.com>
2025-06-04 23:31:48 +0700

    tambah soal

--- src/dao/questionDao.java
+++ src/dao/questionDao.java
@@ -9,5 +9,26 @@ import java.util.List;
 
 public class questionDao {
 
+    // Menambahkan soal baru
+    public static boolean addQuestion(Question question) {
+        String query = "INSERT INTO questions (question_text, option_a, option_b, option_c, option_d, correct_option) VALUES (?, ?, ?, ?, ?, ?)";
+        try (Connection conn = dbConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+
+            stmt.setString(1, question.getQuestionText());
+            stmt.setString(2, question.getOptionA());
+            stmt.setString(3, question.getOptionB());
+            stmt.setString(4, question.getOptionC());
+            stmt.setString(5, question.getOptionD());
+            stmt.setString(6, question.getCorrectOption());
+
+            int rowsAffected = stmt.executeUpdate();
+            return rowsAffected > 0; // Jika berhasil insert
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
     
 }
