99c4a714b66111b511526b180dfa405cc2a12b1e
rdityaa <radityarizki2309@gmail.com>
2025-06-04 23:37:28 +0700

    ambil jawaban dari db

--- src/model/question.java
+++ src/model/question.java
@@ -1,4 +1,13 @@
 package model;
 
-public class question {
+public class Question {
+    private int id;
+    private String questionText;
+    private String optionA;
+    private String optionB;
+    private String optionC;
+    private String optionD;
+    private String correctOption;
+
+
 }
