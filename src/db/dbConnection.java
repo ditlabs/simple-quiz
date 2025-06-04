9f6ff8bf3b48eee6aedd1ad69bc1fea99eebcbaa
rdityaa <radityarizki2309@gmail.com>
2025-06-04 23:33:49 +0700

    import db, model

--- src/db/dbConnection.java
+++ src/db/dbConnection.java
@@ -1,4 +1,18 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class dbConnection {
+    public static Connection getConnection() {
+        try {
+            String url = "jdbc:mysql://localhost/db_quiz"; // Nama Database
+            String user = "root"; // Username
+            String pass = "dit"; // Password
+            return DriverManager.getConnection(url, user, pass);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
