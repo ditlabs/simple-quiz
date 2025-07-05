2099eea732f6290fc9507e4949dcc708d8e007e1
rdityaa <radityarizki2309@gmail.com>
2025-07-06 01:31:10 +0700

    add imagePath field and update constructors in Question class

--- src/model/question.java
+++ src/model/question.java
@@ -3,16 +3,18 @@ package model;
 public class Question {
     private int id;
     private String questionText;
+    private String imagePath; // Field untuk path gambar
     private String optionA;
     private String optionB;
     private String optionC;
     private String optionD;
     private String correctOption;
 
-    // Constructor
-    public Question(int id, String questionText, String optionA, String optionB, String optionC, String optionD, String correctOption) {
+    // Constructor lengkap: untuk mengambil data dari database
+    public Question(int id, String questionText, String imagePath, String optionA, String optionB, String optionC, String optionD, String correctOption) {
         this.id = id;
         this.questionText = questionText;
+        this.imagePath = imagePath;
         this.optionA = optionA;
         this.optionB = optionB;
         this.optionC = optionC;
@@ -20,8 +22,10 @@ public class Question {
         this.correctOption = correctOption;
     }
 
-    public Question(String questionText, String optionA, String optionB, String optionC, String optionD, String correctOption) {
+    // Constructor sederhana: untuk membuat soal baru dari form
+    public Question(String questionText, String imagePath, String optionA, String optionB, String optionC, String optionD, String correctOption) {
         this.questionText = questionText;
+        this.imagePath = imagePath;
         this.optionA = optionA;
         this.optionB = optionB;
         this.optionC = optionC;
@@ -29,60 +33,29 @@ public class Question {
         this.correctOption = correctOption;
     }
 
-    // Getter and Setter methods
-    public int getId() {
-        return id;
-    }
+    // --- Getter dan Setter ---
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
-    public String getQuestionText() {
-        return questionText;
-    }
+    public String getQuestionText() { return questionText; }
+    public void setQuestionText(String questionText) { this.questionText = questionText; }
 
-    public void setQuestionText(String questionText) {
-        this.questionText = questionText;
-    }
+    public String getImagePath() { return imagePath; }
+    public void setImagePath(String imagePath) { this.imagePath = imagePath; }
 
-    public String getOptionA() {
-        return optionA;
-    }
+    public String getOptionA() { return optionA; }
+    public void setOptionA(String optionA) { this.optionA = optionA; }
 
-    public void setOptionA(String optionA) {
-        this.optionA = optionA;
-    }
+    public String getOptionB() { return optionB; }
+    public void setOptionB(String optionB) { this.optionB = optionB; }
 
-    public String getOptionB() {
-        return optionB;
-    }
+    public String getOptionC() { return optionC; }
+    public void setOptionC(String optionC) { this.optionC = optionC; }
 
-    public void setOptionB(String optionB) {
-        this.optionB = optionB;
-    }
+    public String getOptionD() { return optionD; }
+    public void setOptionD(String optionD) { this.optionD = optionD; }
 
-    public String getOptionC() {
-        return optionC;
-    }
-
-    public void setOptionC(String optionC) {
-        this.optionC = optionC;
-    }
-
-    public String getOptionD() {
-        return optionD;
-    }
-
-    public void setOptionD(String optionD) {
-        this.optionD = optionD;
-    }
-
-    public String getCorrectOption() {
-        return correctOption;
-    }
-
-    public void setCorrectOption(String correctOption) {
-        this.correctOption = correctOption;
-    }
+    public String getCorrectOption() { return correctOption; }
+    public void setCorrectOption(String correctOption) { this.correctOption = correctOption; }
 }
